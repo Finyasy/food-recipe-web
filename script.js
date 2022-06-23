8eebd38bb71d34e1a6e534eb17e37da93faa293d
Bryan Bosire <bryanbosire@gmail.com>
2022-06-23 10:51:42 +0300

    declared variables to grab different elements in html

---
+++ script.js
@@ -0,0 +1,4 @@
+const searchButton = document.getElementById('search-btn');
+const foodList =  document.getElementById('food');
+const foodDetailsContent = document.querySelector('.food-details-content');
+const recipeButtonClose = document.getElementById('recipe-close-btn');
